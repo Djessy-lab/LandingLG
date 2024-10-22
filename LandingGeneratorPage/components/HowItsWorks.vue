3e2990ddc3ace5d5684bdcba650cf2e10d3e1747
Djessy Coiffe <djessy.coiffe@gmail.com>
2024-10-22 12:19:35 +0200

    change format video

--- LandingGeneratorPage/components/HowItsWorks.vue
+++ LandingGeneratorPage/components/HowItsWorks.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="sm:w-[100%] lg:w-[60%] mx-auto rounded-3xl shadow-2xl">
     <video autoplay loop muted class="rounded-3xl">
-      <source src="/img/video.mov" type="video/mp4">
+      <source src="/img/video.mp4" type="video/mp4">
     </video>
   </div>
 </template>
